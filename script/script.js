eee3935b3ce23c555b920300adfdbcfeff584840
Marcus Barasukana <makousb@gmail.com>
2016-10-23 08:33:16 +0300

    adding definitions

--- script/script.js
+++ script/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
+  $(".click-1").click(function() {
     $("#Javascript-showing").fadeToggle();
     $("#Javascript-hidden").fadeToggle();
+  });
+  $(".click-2").click(function() {
+    $("#operator-showing").fadeToggle();
+    $("#operator-hidden").fadeToggle();
+  });
+  $(".click-3").click(function() {
+    $("#method-showing").fadeToggle();
+    $("#method-hidden").fadeToggle();
   });
 });
